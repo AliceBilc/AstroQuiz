f0a59a4aef310aae408797ab46cfaf5c3cc77aef
Alice <alice.bilc@gmail.com>
2021-01-22 17:13:29 +0200

    Init Spring project

---
+++ src/main/java/com/devmind/AstroQuiz/AstroQuizApplication.java
@@ -0,0 +1,13 @@
+package com.devmind.AstroQuiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstroQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AstroQuizApplication.class, args);
+	}
+
+}
